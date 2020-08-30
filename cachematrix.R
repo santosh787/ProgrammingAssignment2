22f5c6070a5e55fbd01777c35133e2641b46fff3
santosh787 <70477938+santosh787@users.noreply.github.com>
2020-08-30 11:05:50 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,31 @@
-## Put comments here that give an overall description of what your
-## functions do
+## function creates a special matrix 
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(a = matrix()) {
+  #set a null matrix
+  inv<-NULL
+  set<-function(a){
+    #super-assignment of values in global environment
+    a<<-b
+    inv<<-NULL
+  }
+  get<-function()a
+  calcinverse<-function(inverse) inv<<-inverse
+  retinverse<-function() inv
+  list(set=set,get=get,calcinverse=calcinverse, retinverse=retinverse)
 }
 
 
-## Write a short comment describing this function
+## function computes inverse of special matrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv <- x$retinvers()
+  if (!is.null(inv))
+  {
+    message("inverse already computed; getting cached data")
+    return(inv)
+  }
+        data<-x$get()
+        inv<-solve(data,...)
+        x$calcinverse(inv)
+        return(inv)
 }
